a22818177780502e9a99f32f1f03ddbb9fece39c
gagan30052001 <115109152+gagan30052001@users.noreply.github.com>
2024-09-19 19:54:59 +0530

    Create queries.sql

---
+++ queries.sql
@@ -0,0 +1,7 @@
+SELECT u.user_id, u.username, SUM(o.total_amount) AS total_spend
+FROM users u
+JOIN orders o ON u.user_id = o.user_id
+WHERE o.order_date >= CURRENT_DATE - INTERVAL '30 days'
+GROUP BY u.user_id, u.username
+ORDER BY total_spend DESC
+LIMIT 5;
