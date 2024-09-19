bae9f213b15f43a2d3d4a8fc29af53faef4aaf9f
gagan30052001 <115109152+gagan30052001@users.noreply.github.com>
2024-09-19 20:00:50 +0530

    Update and rename queries.sql to queryA.sql

--- queryA.sql
+++ queryA.sql
@@ -5,9 +5,4 @@ WHERE o.order_date >= CURRENT_DATE - INTERVAL '30 days'
 GROUP BY u.user_id, u.username
 ORDER BY total_spend DESC
 LIMIT 5;
-SELECT p.product_id, p.product_name, SUM(oi.quantity) AS total_quantity
-FROM products p
-JOIN order_items oi ON p.product_id = oi.product_id
-GROUP BY p.product_id, p.product_name
-ORDER BY total_quantity DESC
-LIMIT 1;
+
